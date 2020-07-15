a3b28c3da8d2358a71a109c6d940424b7df80839
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-07-14 21:40:36 -0400

    Initial commit, add README.md, .gitignore & remote

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by SB on 7/14/20.
+//  Copyright © 2020 SB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
