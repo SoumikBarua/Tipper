ab7f6bd17bae6b523689f9344e0903336fd47a19
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-07-15 23:36:44 -0400

    Connect Storyboard elements to ViewController

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var tipAmountSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        
+    }
+    
 }
 
