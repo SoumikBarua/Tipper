566e581c13a59ae3080f7e7cd2050e437a60fdff
SB <10300902+SoumikBarua@users.noreply.github.com>
2020-07-16 19:18:16 -0400

    Add logic to calculate tip based on tip percentage

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -24,7 +24,17 @@ class ViewController: UIViewController {
         
     }
     @IBAction func calculateTip(_ sender: Any) {
+        // Get initial bill amount and calculate tips
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
         
+        // Calculate tip and total
+        let tip = bill * tipPercentages[tipAmountSegmentedControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total labels
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
     
 }
